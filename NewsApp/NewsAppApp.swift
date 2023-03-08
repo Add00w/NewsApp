877eb3601510710a85fd320b9d06996fdd1d625c
Abdullahi Addow <jiincade@gmail.com>
2023-03-08 13:16:28 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Abdullah Addow on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
